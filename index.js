71491f056bf43c8bf193e56c69bee17d190cc971
afianasrin1 <afia.nasrin3e@gmail.com>
2022-11-04 17:37:46 +0600

    67 module end

--- index.js
+++ index.js
@@ -73,8 +73,31 @@ app.get("/services/:id", async (req, res) => {
     });
   }
 });
-
-//orders api(order=req.body likhlam na )
+// all order dekhbo order.js database theke
+app.get("/orders", async (req, res) => {
+  try {
+    let query = {};
+    if (req.query.email) {
+      query = {
+        email: req.query.email,
+      };
+    }
+    const cursor = orderCollection.find(query);
+    const orders = await cursor.toArray();
+    res.send({
+      success: true,
+      message: "successfully got the order data",
+      data: orders,
+    });
+  } catch (error) {
+    console.log(error.name.red, error.message.bold, error.stack);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+//orders api(order=req.body likhlam na )data insert korlam post die database  te
 app.post("/orders", async (req, res) => {
   try {
     const result = await orderCollection.insertOne(req.body);
@@ -90,6 +113,63 @@ app.post("/orders", async (req, res) => {
     }
   } catch (error) {
     console.log(error.name.red, error.message.bold, error.stack);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+app.patch("/orders/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await orderCollection.updateOne(
+      { _id: ObjectId(id) },
+      { $set: req.body }
+    );
+
+    if (result.matchedCount) {
+      res.send({
+        success: true,
+        message: `successfully updated ${req.body.name}`,
+      });
+    } else {
+      res.send({
+        success: false,
+        error: "Couldn't update  the product",
+      });
+    }
+  } catch (error) {
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+app.delete("/orders/:id", async (req, res) => {
+  try {
+    //kontake delete korsi name saho caile 142line must  dibo 141 dibo na,eki jinish 2nd toime delet nakorar janno emonvabe korbo(2.14mint e)
+    // const product = await orders.findOne({ _id: ObjectId(id) });
+
+    // if (!product?._id) {
+    //   res.send({
+    //     success: false,
+    //     error: "Product doesn't exist",
+    //   });
+    //   return;
+    // }
+    const { id } = req.params;
+    const result = await orderCollection.deleteOne({ _id: ObjectId(id) });
+
+    if (result.deletedCount) {
+      res.send({
+        success: true,
+        message: `Successfully deleted the the order`,
+        // message: `Successfully deleted the ${order.name}`,
+      });
+    } else {
+    }
+  } catch (error) {
     res.send({
       success: false,
       error: error.message,
